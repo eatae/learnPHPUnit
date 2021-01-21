06a811354be7076a02e96405d736f024bd2c6f32
Eatae <al-loco@mail.ru>
2021-01-22 01:54:46 +0300

    first Test

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <?php
+
+use App\Models\User;
+
 require "vendor/autoload.php";
 
 echo 'Hello';
+
+$user = new User('TestName', 'test@email.com', '1234', 23);
+
+var_dump($user);
---
+++ src/Models/User.php
@@ -0,0 +1,97 @@
+<?php
+
+namespace App\Models;
+
+
+class User
+{
+    private $name;
+    private $email;
+    private $pass;
+    private $age;
+
+    /**
+     * User constructor.
+     * @param $name
+     * @param $email
+     * @param $pass
+     * @param $age
+     */
+    public function __construct($name, $email, $pass, $age)
+    {
+        $this->name = $name;
+        $this->email = $email;
+        $this->pass = $pass;
+        $this->age = $age;
+    }
+
+
+    /**
+     * Name
+     *
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    /**
+     * @param mixed $name
+     */
+    public function setName($name): void
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Email
+     *
+     * @return mixed
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+    /**
+     * @param mixed $email
+     */
+    public function setEmail($email): void
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Pass
+     *
+     * @return mixed
+     */
+    public function getPass()
+    {
+        return $this->pass;
+    }
+    /**
+     * @param mixed $pass
+     */
+    public function setPass($pass): void
+    {
+        $this->pass = $pass;
+    }
+
+    /**
+     * Age
+     *
+     * @return mixed
+     */
+    public function getAge()
+    {
+        return $this->age;
+    }
+    /**
+     * @param mixed $age
+     */
+    public function setAge($age): void
+    {
+        $this->age = $age;
+    }
+
+}
